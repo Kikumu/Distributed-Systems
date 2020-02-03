d8242a237054d0c8248dce47cefb7d99a3866612
Ten5ei <scowty.sk@outlook.com>
2020-02-03 22:06:52 +0000

    practice data

---
+++ Server/Client/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        //public static byte[] Serialize(string Message, int EncryptionKey)
+        //{
+        //    byte[] rawdata;
+        //    return rawdata;
+        //}
+        static void Main(string[] args)
+        {
+            string host_name;
+            int port = 0;
+            //string Message;
+
+            Console.Write("Please enter IP/host address: ");
+            host_name = Console.ReadLine();
+            Console.Write("Please enter port number: ");
+            try
+            {
+                port = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a number.");
+            }
+            TcpClient client = new TcpClient();
+            try
+            {
+                string message;
+                Console.WriteLine("Connecting...");
+                client.Connect(host_name, port);
+                StreamWriter sw = new StreamWriter(client.GetStream());
+                StreamReader sr = new StreamReader(client.GetStream());
+                Console.WriteLine("Connected to server");
+                Console.WriteLine("Please enter message: ");
+                message = Console.ReadLine();
+                sw.WriteLine(message);
+                sw.Flush();
+                string line = sr.ReadLine();
+                Console.WriteLine("Server response: " + line);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+
+           
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Server
+{
+    class Program
+    {
+        public static TcpListener listener; //server(needs to be assigned port and ip)
+        public static void startServer()
+        {
+            Decryptor decryptor;
+            Console.Write("Initializing...");
+            int port = 43;
+            IPAddress local = IPAddress.Parse("127.0.0.1"); //my ip
+            Socket server_socket; //socket is linked with ip
+            listener = new TcpListener(local, port);
+            listener.Start();
+            Console.WriteLine("Server Active.");
+
+            while (true)
+            {
+                try
+                {
+                    server_socket = listener.AcceptSocket(); //allows server to connect
+                    decryptor = new Decryptor();
+                    decryptor.decrypt_client(server_socket);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Unhandled Exception: " + ex);
+                }
+            }
+        }
+        class Decryptor
+        {
+            public void decrypt_client(Socket socket)
+            {
+                NetworkStream server_stream;
+                server_stream = new NetworkStream(socket);
+                try
+                {
+                    Console.WriteLine("Connection to client Established.");
+                    StreamWriter sw = new StreamWriter(server_stream);
+                    StreamReader sr = new StreamReader(server_stream);
+                    string line;
+                    line = sr.ReadLine(); 
+                    Console.WriteLine("Response: " + line);
+                    sw.WriteLine("Received");
+                    sw.Flush();
+                        }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unhandled exception: " + ex);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+           
+            startServer();
+            
+        }
+
+          
+        
+    }
+}
