0d4b976bbe93028e9ebbab829765dab2be6dc972
Ten5ei <scowty.sk@outlook.com>
2020-02-06 11:02:07 +0000

    lab tutorial

---
+++ Server/Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace System.Runtime.Remoting
+{
+
+}
+
+namespace System.Runtime.Remoting.Channels
+{
+
+}
+
+namespace System.Runtime.Remoting.Channels.Tcp
+{
+
+}
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpChannel channel = new TcpChannel(43);
+            ChannelServices.RegisterChannel(channel, false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(translator), "Translate", WellKnownObjectMode.SingleCall);
+            Console.WriteLine("Press any key to exit...");
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Test/translator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class translator:MarshalByRefObject
+    {
+        public string Translate(string English_string)
+        {
+            string[] words = English_string.Split(' ');
+            string result = "";
+            foreach(string word in words)
+            {
+                result = word.Substring(1);
+                result += word.Substring(0, 1) + "ay";
+            }
+            return result;
+        }
+        
+    }
+}
